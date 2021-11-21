8e5a55755acfb5c9d6abd99f6d819103ccd5b6be
Raine Rantanen <raine.ran7anen@gmail.com>
2021-11-21 19:18:59 +0200

    6.13-6.14 done

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,60 +1,43 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-];
-
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
-
-
 // Action creators
 export const voteAnecdote = (id) => {
   return {
     type: 'VOTE_ANECDOTE',
-    data: {
-      id,
-    },
+    data: id,
   };
 };
 
-export const addAnecdote = (content) => {
+export const addAnecdote = (anecdote) => {
   return {
     type: 'ADD_ANECDOTE',
-    data: {
-      content: content,
-    },
+    data: anecdote
+  };
+};
+
+export const initAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes
   };
 };
 
 // Reducer
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   console.log('state now: ', state);
   console.log('action', action);
 
   switch (action.type) {
     case 'VOTE_ANECDOTE':
       return state.map((anecdote) => {
-        if (anecdote.id === action.data.id) {
+        if (anecdote.id === action.data) {
           return { ...anecdote, votes: (anecdote.votes += 1) };
         } else {
           return anecdote;
         }
       });
     case 'ADD_ANECDOTE':
-      return [...state, asObject(action.data.content)];
+      return [...state, action.data];
+    case 'INIT_ANECDOTES':
+      return action.data;
     default:
       return state;
   }
