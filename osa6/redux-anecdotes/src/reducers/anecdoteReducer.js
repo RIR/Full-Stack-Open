8af7ed56758b6e443e058676b8fe11faabb8cff1
Raine Rantanen <raine.ran7anen@gmail.com>
2021-11-21 19:38:04 +0200

    6.15-6.16 done

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,25 +1,34 @@
+import anecdoteService from '../services/anecdotes';
+
 // Action creators
 export const voteAnecdote = (id) => {
-  return {
-    type: 'VOTE_ANECDOTE',
-    data: id,
+  return async (dispatch) => {
+    dispatch({
+      type: 'VOTE_ANECDOTE',
+      data: id,
+    });
   };
 };
 
-export const addAnecdote = (anecdote) => {
-  return {
-    type: 'ADD_ANECDOTE',
-    data: anecdote
+export const addAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.createNew(content);
+    dispatch({
+      type: 'ADD_ANECDOTE',
+      data: newAnecdote,
+    });
   };
 };
 
-export const initAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes
+export const initAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes,
+    });
   };
 };
-
 // Reducer
 const reducer = (state = [], action) => {
   console.log('state now: ', state);
